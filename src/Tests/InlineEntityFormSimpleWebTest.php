91c9eeda5cdf93e82f511d918ed2c790219d4f26
tedbow <tedbow@240860.no-reply.drupal.org>
2016-02-18 20:19:09 +0100

    Issue #2668638 by tedbow: Remove Test function testCustomFormIEF from InlineEntityFormSimpleWebTest

--- src/Tests/InlineEntityFormSimpleWebTest.php
+++ src/Tests/InlineEntityFormSimpleWebTest.php
@@ -56,24 +56,6 @@ class InlineEntityFormSimpleWebTest extends InlineEntityFormTestBase {
   }
 
   /**
-   * Tests IEF on a custom form.
-   */
-  public function testCustomFormIEF() {
-    $this->drupalGet('ief-test');
-    $this->assertText(t('Title'), 'Title field found on the form.');
-
-    $edit = ['inline_entity_form[title][0][value]' => $this->randomString()];
-    $this->drupalPostForm('ief-test', $edit, t('Save'));
-    $message = t('Created @entity_type @label.', ['@entity_type' => t('Content'), '@label' => $edit['inline_entity_form[title][0][value]']]);
-    $this->assertText($message, 'Status message found on the page.');
-
-    /** @var \Drupal\node\NodeInterface $node */
-    $node = $this->container->get('entity.manager')->getStorage('node')->load(1);
-    $this->assertEqual($node->label(), $edit['inline_entity_form[title][0][value]'], 'Node title correctly saved to the database.');
-    $this->assertEqual($node->bundle(), 'ief_test_custom', 'Correct bundle used when creating the new node.');
-  }
-
-  /**
    * Tests simple IEF widget with different cardinality options.
    *
    * @throws \Exception
